85a089a35c5bf0ab8ece44e557ad295bc5fd4cfb
ffeigl <f.feigl@gbs-technikum.de>
2019-03-28 12:30:25 +0100

    Add generateCode

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-function testHandler () {
-	console.log('test');
-}
-
-function tdClickHandler(){
-	var farbe = colorSwitch(this.style.backgroundColor);
-	this.style.backgroundColor = farbe;
-}
-
-function colorSwitch(color){
-	switch(color){
-		case 'red':
-			return 'blue';
-			break;
-		case 'blue':
-			return 'green';
-			break;
-		case 'green':
-			return 'yellow';
-			break;
-		case 'yellow':
-			return 'black';
-			break;
-		case 'black':
-			return 'grey';
-			break;
-		case 'grey':
-			return 'pink';
-			break;
-		default:
-			return 'red';
-	}
+function init(){
+	addTable();
+	addButton();
 }
 
 function addTable() {
@@ -83,11 +54,139 @@ function addTable() {
   myTableDiv.appendChild(table);
 }
 
+function addButton(){
+	var buttonDiv = document.getElementById('btnCheck');
+	
+	var button = document.createElement('INPUT');
+	button.type = 'button';
+	button.value = 'Check!';
+	button.onclick = btnCheckHandler;
+	
+	buttonDiv.appendChild(button);
+}
+
+function generateCode(){
+	if(columns == 4){
+		code = [0,0,0,0];
+	} else {
+		code = [0,0,0,0,0];
+	}
+	for(var i = 0; i < code.length; i++){
+		generateNumber(i);
+	}
+	convertNumbersToColors();
+}
+
+function generateNumber(i){
+	code[i] = parseInt(Math.random()*colors);
+	checkNumber(i);
+}
+
+function checkNumber(i){
+	for(var j = i-1; j >= 0; j--){
+		if(i != 0){
+			if(code[i] == code[j]){
+				generateNumber(i);
+			}
+		}
+	}
+}
+
+function convertNumbersToColors(){
+	for(var i = 0; i < code.length; i++){
+		code[i] = assignColor(code[i]);
+	}
+}
+
+function assignColor(i){
+	switch(i){
+		case 0:
+			return 'red';
+			break;
+		case 1:
+			return 'blue';
+			break;
+		case 2:
+			return 'green';
+			break;
+		case 3:
+			return 'yellow';
+			break;
+		case 4:
+			return 'black';
+			break;
+		case 5:
+			return 'grey';
+			break;
+		case 6:
+			return 'pink';
+			break;
+		case 7:
+			return 'brown';
+	}
+}
+
+function codeAusgeben(){
+	for(var i = 0; i < code.length; i++){
+		console.log(code[i]);
+	}
+}
+
+function colorSwitch(color){
+	switch(color){
+		case 'red':
+			return 'blue';
+			break;
+		case 'blue':
+			return 'green';
+			break;
+		case 'green':
+			return 'yellow';
+			break;
+		case 'yellow':
+			return 'black';
+			break;
+		case 'black':
+			return 'grey';
+			break;
+		case 'grey':
+			if(columns == 4){
+				return 'red';
+			}else{
+				return 'pink';
+			}
+			break;
+		case 'pink':
+			return 'brown';
+			break;
+		case 'brown':
+			return 'red';
+			break;
+		default:
+			return 'red';
+	}
+}
+
+// Eventhandler
+function tdClickHandler(){
+	this.style.backgroundColor = colorSwitch(this.style.backgroundColor);
+}
+
+function btnCheckHandler(){
+	console.log("Click");
+}
+
+// START
 var rows = 8;
 var columns = 4;
+var colors = 6;
 
-addTable();
+var code;
 
+init();
+
+generateCode();
+codeAusgeben();
 
 var tds = document.getElementsByClassName('action');
 
